a714bff1232be3a12d9ffd004548f6369dc399ee
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-08 16:49:14 -0500

    can now add items to the list

--- todo-reducer/src/components/Displayed-Items.js
+++ todo-reducer/src/components/Displayed-Items.js
@@ -1,14 +1,35 @@
 import React, { useState, useReducer } from 'react';
+import { initialState, reducer } from '../reducers/reducer';
 
-import { initialState, reducer } from '../reducers/reducer'
+import ItemForm from './form'
 
 const ItemList = () => {
-    const [{item, completed}, dispatch] = useReducer(reducer, initialState);
-    const [newItem, setNewItem] = useState({});
-
+    const [{item, completed, itemsList}, dispatch] = useReducer(reducer, initialState);
+    const [newItem, setNewItem] = useState('');
+    const handleChanges = e => {
+        setNewItem(e.target.value)
+    }
     return (
       <div>
+           <>
+            <form>
+                <input
+                  type="text"
+                  name="newItem"
+                  value={newItem}
+                  onChange={handleChanges}
+                />
+            </form>
+            <button type='submit' onClick={() => 
+            dispatch({ type: 'ADD_ITEM', payload: newItem })}>Add Item</button>
+        </>
           <p>{item}</p>
+          {itemsList.map(i => {
+              console.log(itemsList)
+              return(
+                  <p>{i.item}</p>
+              )
+          })}
       </div>  
     )
 }
--- todo-reducer/src/reducers/reducer.js
+++ todo-reducer/src/reducers/reducer.js
@@ -1,11 +1,22 @@
 export const initialState = {
     item: 'Learn about reducers',
     completed: false,
-    id: 3892987589
+    id: 3892987589,
+    itemsList: []
 }
 
 export const reducer = (state, action) => {
     switch (action.type){
+        case 'ADD_ITEM':
+            const newItem = {
+                item: action.payload,
+                completed: false,
+                id: Date.now()
+            };
+            return {
+                ...state,
+                itemsList: [...state.itemsList, newItem]
+            };
         default:
             return state;
     }
